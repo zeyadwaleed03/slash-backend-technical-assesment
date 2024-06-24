dc75b6841f8e37388de78e451e2c4047ee01ca7c
Zeyad Waleed Amin <ziadwalid20035@gmail.com>
2024-06-24 23:55:16 +0300

    fix: add coupon to order responses

--- src/order/swaggerResponses/order.ts
+++ src/order/swaggerResponses/order.ts
@@ -17,6 +17,8 @@ export class OrderResponse {
 
   @ApiProperty({ example: 200.0 })
   totalPrice: number;
+  @ApiProperty({ example: 20 })
+  coupon: number;
 
   @ApiProperty({ example: 1 })
   userId: number;
